e10349c4af38812f75480ea55b0192c298ab7acb
Abhilasha <abhilashas71@gmail.com>
2020-08-06 13:40:19 +0530

    new classes added

---
+++ src/main/java/com/ofss/TestProduct.java
@@ -0,0 +1,12 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		Product p= new Product("Books", 100.00);
+		System.out.println("Hi");
+		System.out.println("integartion with jenkins and GIT");
+
+	}
+
+}

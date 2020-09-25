eda08d73ae4f22044dca417af5b5cd54ed40c070
abhilashas71 <abhilashas71@gmail.com>
2020-09-25 17:59:02 +0530

    Update EngAuthor.java

--- src/main/java/com/ofss/EngAuthor.java
+++ src/main/java/com/ofss/EngAuthor.java
@@ -4,7 +4,7 @@ public class EngAuthor {
 
 	public static void main(String[] args) {
 		System.out.println("English Authors");
-		System.out.println("French Authors ");
+		System.out.println("French Authors");
 
 	}
 
